566543c1dc95ff14853319fef851607d2087fc4f
八門 <chenzhzh45@gmail.com>
2017-08-01 10:23:54 +0900

    Praise the Sun!

--- js/pagescode.js
+++ js/pagescode.js
@@ -1,6 +1,41 @@
 var pages=new Array(5);
-pages[0]='<h1>Aniki-Chen</h1><p>This page still under constructing</p><nav id="lister"><ul><li><a href="https://github.com/Aniki-Chen" class="icon fa-github" title="Github"></a></li><li><a href="http://weibo.com/u/1237000977" class="icon fa-weibo" title="微博"></a></li><li><a href="mailto:chenzhzh45@gmail.com" class="icon fa-envelope-o" title="Email"></a></li></ul></nav><p>Using IE/Edge browse this page would be strange</p><a class="featured-post-link" onclick="changebg()" class="icon" title=""><img class="featured-post-image" alt="change color" /></a>';
-pages[1]='<h1>This is page A</h1><p>For demos, please scroll down</p>';
-pages[2]='<h1>This is page B</h1><p>Continue scroll down</p>';
-pages[3]='<h1>This is page C</h1><p>Still scroll down</p>';
-pages[4]='<h1>Page Demo</h1><p>Oops! DEMOs havent put up yet!</p>';
+var pagesEn=new Array(5);
+var pagesZh=new Array(5);
+var lang="En"
+var switcher=document.getElementById("switcher");
+pagesEn[0]='<h1>Aniki-Chen</h1><p>This page still under constructing</p><nav id="lister"><ul><li><a href="https://github.com/Aniki-Chen" class="icon fa-github" title="Github"></a></li><li><a href="http://weibo.com/u/1237000977" class="icon fa-weibo" title="weibo"></a></li><li><a href="mailto:chenzhzh45@gmail.com" class="icon fa-envelope-o" title="Email"></a></li></ul></nav><p>Using IE/Edge browse this page would be strange<br/>Try refresh</p>';
+pagesEn[1]='<h1>This is page A</h1><p>For demos, please scroll down</p>';
+pagesEn[2]='<h1>This is page B</h1><p>Continue scroll down</p>';
+pagesEn[3]='<h1>This is page C</h1><p>Still scroll down</p>';
+pagesEn[4]='<h1>Page Demo</h1><p>Oops! DEMOs havent put up yet!</p>';
+
+pagesZh[0]='<h1>中哥</h1><p>页面还在建设中啊</p><nav id="lister"><ul><li><a href="https://github.com/Aniki-Chen" class="icon fa-github" title="鸡特哈不"></a></li><li><a href="http://weibo.com/u/1237000977" class="icon fa-weibo" title="微博"></a></li><li><a href="mailto:chenzhzh45@gmail.com" class="icon fa-envelope-o" title="伊妹儿"></a></li></ul></nav><p>用IE/Edge浏览这个可能会看起来怪怪的<br/>再刷新一次试试吧</p>';
+pagesZh[1]='<h1>这里是页面 A</h1><p>想看DEMO的话，请往下翻</p>';
+pagesZh[2]='<h1>这里是页面B</h1><p>继续往下翻</p>';
+pagesZh[3]='<h1>这里是页面C</h1><p>还是往下翻</p>';
+pagesZh[4]='<h1>Demo页</h1><p>哎哟喂！DEMO还没放上来呢</p>';
+
+for (var i=0;i<5;i++) {
+	pages[i]=pagesEn[i];
+}
+
+function langswitch (){
+	if (lang=="En") {
+		for (var i=0;i<5;i++) { pages[i]=pagesZh[i]; }
+		switcher.className="icon fa-toggle-off";
+		switcher.innerHTML=' 中'
+		lang="Zh"
+	} else{
+		for (var i=0;i<5;i++) { pages[i]=pagesEn[i]; }
+		switcher.className="icon fa-toggle-on";
+		switcher.innerHTML=' En'
+		lang="En"
+	}
+	scrollprogressa(false,true);
+}
+ function iniswitcher (){
+ 	switcher.onclick=function (){ langswitch(); }
+ }
+ function clearswitcher (){
+ 	switcher.onclick=function (){ return; }
+ }
--- js/pagescroll.js
+++ js/pagescroll.js
@@ -1,14 +1,59 @@
 var currentpage="navhome";
 var currentnum=0;
+var aimnum=0;
 var headerpos=0;
 var headeropa=1;
 document.getElementById(currentpage).style.backgroundColor="rgba(255, 255, 255, 0.35)";
 
+var colors = new Array(
+	[0,127,127],
+	[0,0,255],
+	[127,0,255],
+	[255,0,0],
+	[255,127,0]);
+var cR=colors[0][0], cG=colors[0][1], cB=colors[0][2];
+var currentcolor="rgb("+cR+","+cG+","+cB+")";
+document.getElementById("bgcolor").style.backgroundColor=currentcolor;
 
-function pagefocus(aimpage){
+function bgfader () {
+	if (cR==colors[aimnum][0] && cG==colors[aimnum][1] && cB==colors[aimnum][2]) {
+		return;
+	} else {
+		if (cR<colors[aimnum][0]) {cR++;}
+		if (cR>colors[aimnum][0]) {cR--;}
+		if (cG<colors[aimnum][1]) {cG++;}
+		if (cG>colors[aimnum][1]) {cG--;}
+		if (cB<colors[aimnum][2]) {cB++;}
+		if (cB>colors[aimnum][2]) {cB--;}
+		currentcolor="rgb("+cR+","+cG+","+cB+")";
+		document.getElementById("bgcolor").style.backgroundColor=currentcolor;
+		setTimeout("bgfader()",10);
+	}
+}
+
+function pagefocus(aim){
+	var aimpage;
+	switch (aim){
+		case 0:
+			aimpage="navhome";
+			break;
+		case 1:
+			aimpage="nava";
+			break;
+		case 2:
+			aimpage="navb";
+			break;
+		case 3:
+			aimpage="navc";
+			break;
+		case 4:
+			aimpage="navd";
+			break;
+	}
 	document.getElementById(currentpage).style.backgroundColor="transparent";
 	currentpage=aimpage;
 	document.getElementById(currentpage).style.backgroundColor="rgba(255, 255, 255, 0.35)";
+	return aimpage;
 }
 function inimousewheel(){
 	//浏览器兼容
@@ -51,93 +96,136 @@ function scroller(event){
 		if (currentnum==4) {
 			return;
 		}
-		scrollprogressa(true);
+	//滚动翻页双向清除很重要
 		clearmousewheel();
+		clearnaver();
+		clearswitcher();
+		scrollprogressa(true);
 	}
 	if(delta<0){
 		//向上滚动
 		if (currentnum==0) {
 			return;
 		}
-		scrollprogressa(false);
+	//重要的事情再说一遍
 		clearmousewheel();
+		clearnaver();
+		clearswitcher();
+		scrollprogressa(false);
 	}
 }
+function jumppage(aim){
+	aimnum=aim;
+	scrollprogressa(null,true);
+	//重要的事情说三遍
+	clearnaver();
+	clearmousewheel();
+	clearswitcher();
+}
 
-function scrollprogressa(dir){
-	if (dir) {
-			headerpos-=0.5;
-		} else{
-			headerpos+=0.5;
-		}
-		headeropa-=0.01;
+function scrollprogressa(dir,jump){
+	if (!jump) {
+		if (dir) {
+				headerpos-=0.5;
+			} else{
+				headerpos+=0.5;
+			}
 		document.getElementById("header").style.top=headerpos+'%';
+	}
+		headeropa-=0.01;
 		document.getElementById("header").style.opacity=headeropa;
 	if (headeropa>0) {
-		if (dir) {
-			setTimeout("scrollprogressa(true)",5);
+		if (!jump) {
+			if (dir) {
+				setTimeout("scrollprogressa(true)",5);
+			} else{
+				setTimeout("scrollprogressa(false)",5);
+			}
 		} else{
-			setTimeout("scrollprogressa(false)",5);
+			if (dir) {
+				setTimeout("scrollprogressa(true,true)",5);
+			} else{
+				setTimeout("scrollprogressa(false,true)",5);
+			}
 		}
 	} else{
 		headeropa=0;
 		document.getElementById("header").style.opacity=headeropa;
-		if (dir) {
-			headerpos=50;
-			document.getElementById("header").style.top=headerpos+'%';
-		} else{
-			headerpos=-50;
-			document.getElementById("header").style.top=headerpos+'%';
+		if (!jump) {
+			if (dir) {
+				headerpos=50;
+				document.getElementById("header").style.top=headerpos+'%';
+			} else{
+				headerpos=-50;
+				document.getElementById("header").style.top=headerpos+'%';
+			}
 		}
-		scrollprogressb(dir);
+		scrollprogressb(dir,jump);
 	}
 }
-function scrollprogressb(dir){
-	if (dir) {
-		var aimnum=++currentnum;
-	} else{
-		var aimnum=--currentnum;
+function scrollprogressb(dir,jump){
+	if (!jump){
+		if (dir) {
+			aimnum=currentnum+1;
+		} else{
+			aimnum=currentnum-1;
+		}
 	}
-	switch (aimnum){
-		case 0:
-			pagefocus("navhome");
-			break;
-		case 1:
-			pagefocus("nava");
-			break;
-		case 2:
-			pagefocus("navb");
-			break;
-		case 3:
-			pagefocus("navc");
-			break;
-		case 4:
-			pagefocus("navd");
-			break;
-	}
+	bgfader(aimnum);
+	pagefocus(aimnum);
 	document.getElementById("header").innerHTML=pages[aimnum];
-	scrollprogressc(dir);
+	currentnum=aimnum;
+	scrollprogressc(dir,jump);
 }
-function scrollprogressc(dir){
-	if (dir) {
+function scrollprogressc(dir,jump){
+	if (!jump) {
+		if (dir) {
 			headerpos-=0.5;
 		} else{
 			headerpos+=0.5;
 		}
-		headeropa+=0.01;
 		document.getElementById("header").style.top=headerpos+'%';
-		document.getElementById("header").style.opacity=headeropa;
+	}
+	headeropa+=0.01;
+	document.getElementById("header").style.opacity=headeropa;
 	if (headeropa<1) {
-		if (dir) {
-			setTimeout("scrollprogressc(true)",5);
+		if (!jump) {
+			if (dir) {
+				setTimeout("scrollprogressc(true)",5);
+			} else{
+				setTimeout("scrollprogressc(false)",5);
+			}
 		} else{
-			setTimeout("scrollprogressc(false)",5);
+			if (dir) {
+				setTimeout("scrollprogressc(true,true)",5);
+			} else{
+				setTimeout("scrollprogressc(false,true)",5);
+			}
 		}
 	} else{
+		if (!jump) {
+			headerpos=0;
+			document.getElementById("header").style.top=headerpos+'%';
+		}
 		headeropa=1;
 		document.getElementById("header").style.opacity=headeropa;
-		headerpos=0;
-		document.getElementById("header").style.top=headerpos+'%';
 		inimousewheel();
+		ininaver();
+		iniswitcher();
 	}
+}
+
+function ininaver(){
+	document.getElementById("navhome").onclick=function(){jumppage(0)};
+	document.getElementById("nava").onclick=function(){jumppage(1)};
+	document.getElementById("navb").onclick=function(){jumppage(2)};
+	document.getElementById("navc").onclick=function(){jumppage(3)};
+	document.getElementById("navd").onclick=function(){jumppage(4)};
+}
+function clearnaver(){
+	document.getElementById("navhome").onclick=function(){return};
+	document.getElementById("nava").onclick=function(){return};
+	document.getElementById("navb").onclick=function(){return};
+	document.getElementById("navc").onclick=function(){return};
+	document.getElementById("navd").onclick=function(){return};
 }
